8ae10afe303d5be2653a3b12330bc7abff72de16
lMoretl <89445826+lMoretl@users.noreply.github.com>
2024-06-21 15:33:52 +0500

    Lab6

---
+++ Lab6/first.js
@@ -0,0 +1,10 @@
+import React from 'C:\Program Files\nodejs'
+
+import App from 'my-app.jsx'
+import { createRoot } from ''
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(
+    
+)
